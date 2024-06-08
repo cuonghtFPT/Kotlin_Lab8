99b5c6b73aee2486589ceaf4f572cddcf727e4bb
cuonghtph34430 <cuonghtph34430@fpt.edu.vn>
2024-06-08 14:42:42 +0700

    Lab8

---
+++ app/src/main/java/cuonghtph34430/poly/kotlin_lab8/MainActivity.kt
@@ -0,0 +1,48 @@
+package cuonghtph34430.poly.kotlin_lab8
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.kot104.ui.screen.ScreenNavigation
+import cuonghtph34430.poly.kotlin_lab8.ui.theme.Kotlin_lab8Theme
+
+class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Kotlin_lab8Theme {
+                // A surface container using the 'background' color from the theme
+                Scaffold(modifier = Modifier.fillMaxSize().safeDrawingPadding()) {
+                    ScreenNavigation()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Kotlin_lab8Theme {
+        Greeting("Android")
+    }
+}
